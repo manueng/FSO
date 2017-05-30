77d1a6c492eb81a4074552a5e2514f37460489b8
Emanuel Holanda Barroso <manusenac@hotmail.com>
2017-05-30 14:23:40 -0300

    linkagem dinamicaa

---
+++ trab04/qb/include/funcoesmatemati.h
@@ -0,0 +1,6 @@
+#ifndef FUNCOESMATEMATICAS_H
+#define FUNCOESMATEMATI_H
+ double seno(int x);
+ double arcseno( double n);
+#endif 
+
---
+++ trab04/qb/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "funcoesmath.h" 
+#include "funcoesmatemati.h"
+
+int main()
+{
+	printf("%d\n ",pot(2,4));
+	printf("%d\n",fatorial(10));
+    printf("%d\n",calcn(20));
+    printf("%lf\n",seno(60.0));
+	return 0;
+}
