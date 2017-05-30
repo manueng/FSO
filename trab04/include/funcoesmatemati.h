92bdb2c4b1e966a714aec690224c6422c9f3c433
Emanuel Holanda Barroso <manusenac@hotmail.com>
2017-05-30 14:26:19 -0300

    removendo duplicados

--- trab04/include/funcoesmatemati.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUNCOESMATEMATICAS_H
-#define FUNCOESMATEMATI_H
- double seno(int x);
- double arcseno( double n);
-#endif 
-
--- trab04/include/funcoesmath.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNCOESMATH_H
-#define FUNCOESMATH_H
- int fatorial(int numero);
- int pot( int numero, int potencia);
- int  calcn(int n);
-#endif 
-
