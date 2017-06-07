47c14125676f5b252525abe5d67538ca42b49d1f
manueng <manusenac@hotmail.com>
2017-06-06 21:02:34 -0400

    salvando o arquivo no repositorio

---
+++ trab04/trab05/trabalho5inicio.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ int main(  int argc , char* argv[]){
+ 
+ 
+  if(argc==3){
+    printf("numero de prametros valido");
+  }else{
+    printf("numero de parametros invalido");
+  
+ 
+ }
+ }
