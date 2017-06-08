3e88220f9ed86111ed299ca340bf1c140f01ef67
Emanuel Holanda Barroso <manusenac@hotmail.com>
2017-06-08 13:19:18 -0300

    inserindo estrtura do trabalho 5

---
+++ trab05/src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc , char* argv[])
+{
+	if(argc==4)
+	{
+		printf("numero de prametros valido\n");
+	}
+	else
+	{
+		printf("numero de parametros invalido\n");
+	}
+	return 0;
+}
