d4e4160dc25995734763564df71f3ab40ef58ad0
Emanuel Holanda Barroso <manusenac@hotmail.com>
2017-06-08 12:27:40 -0300

    organizando pastas

--- trab05/trabalho5inicio.c
+++ trab05/trabalho5inicio.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include<stdlib.h>
  int main(  int argc , char* argv[]){
+ 
+ 
   if(argc==3){
     printf("numero de prametros valido");
-    system("ls");
   }else{
     printf("numero de parametros invalido");
   
